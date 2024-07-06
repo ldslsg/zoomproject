626abc4bfac42ce50545d9164def925924d36ee1
SeungGyu Lee <114027520+ldslsg@users.noreply.github.com>
2024-07-06 02:04:03 -0700

    chat working

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,56 @@
-hello;
+const ul = document.querySelector("ul");
+const nickform = document.querySelector("#nick");
+const messageform = document.querySelector("#message");
+
+const socket = new WebSocket(`wss://${window.location.host}`);
+
+function makeMessage(type, payload) {
+    const msg = { type, payload};
+    return JSON.stringify(msg);
+}
+
+function handleOpen() {
+    console.log("Connected to Server");
+}
+socket.addEventListener("open", handleOpen);
+
+socket.addEventListener("message", (message) => {
+    if (message.data instanceof Blob) {
+        const reader = new FileReader();
+        reader.onload = function() {
+            const li = document.createElement("li");
+            li.innerText = reader.result;
+            ul.append(li);
+        };
+        reader.readAsText(message.data);
+    } else {
+        const li = document.createElement("li");
+        li.innerText = message.data;
+        ul.append(li);
+    }
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from the Browser");
+});
+
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageform.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    const li = document.createElement("li");
+    li.innerText = `You: ${input.value}`;
+    messageList.append(li);
+    input.value = "";
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickform.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    input.value="";
+}
+
+messageform.addEventListener("submit", handleSubmit);
+nickform.addEventListener("submit", handleNickSubmit);
+
--- src/server.js
+++ src/server.js
@@ -1,12 +1,43 @@
+import http from "http";
+import {WebSocketServer} from "ws";
 import express from "express";
+import path from "path";
+import WebSocket from "ws";
 
 const app = express();
 
 app.set('view engine', "pug")
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
+app.set("views", path.join(__dirname, "/views"));
+app.use("/public", express.static(path.join(__dirname, "/public")));
 app.get("/",(req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () =>console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen);
+const handleListen = () => console.log('Listening on http://localhost:3000');
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({server});
+
+function onSocketClose() {
+    console.log("Disconnected from the Browser");
+}
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    socket["nickname"] = "Anon";
+    console.log("Connected to Browser");
+    socket.on("close", onSocketClose);
+    socket.on("message", (msg) => {
+        const parsed = JSON.parse(msg);
+        switch(parsed.type){
+            case "new_message":
+                wss.clients.forEach(client => {if(client.readyState === WebSocket.OPEN) {client.send(`${socket.nickname}: ${parsed.payload}`);}});
+                break;
+            case "nickname":
+                socket["nickname"] = parsed.payload;
+                break;
+        }     
+    });
+});
+
+server.listen(3000, handleListen);
